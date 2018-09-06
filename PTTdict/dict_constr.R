5ebea3d19b951aa3041c438c58e2032a0960e453
liao <liaomovie2@gmail.com>
2018-09-06 11:57:50 +0800

    revert Revert "revert"
    This reverts commit 09195690801528657cb6c71622f3b0f060bb7942.

---
+++ PTTdict/dict_constr.R
@@ -0,0 +1,23 @@
+library(jsonlite)
+library(dplyr)
+
+dat <- fromJSON("dict.json")
+
+# Simplify data structure ----
+idx <- which(colnames(dat) %in% c("title", "url", "date"))
+dat[, idx] <- vapply(dat[, idx], unlist, rep("", nrow(dat)))
+
+# Extract terms from list columns in 'dat' ----
+lst <- vector("list", 3L)
+k <- 1
+for (i in c("bold", "bracket", "link_new")) {
+  lst[[k]] <- data.frame(term = unlist(dat[[i]]),
+                         source = i,
+                         stringsAsFactors = F)
+  k <- k + 1
+}
+lst <- bind_rows(lst)
+
+# Construct dictionary ----
+dict <- filter(lst, nchar(term) <= 15) %>%
+  distinct(term, .keep_all = T)
