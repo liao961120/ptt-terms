dd812990cde3bba02e63ddacfaf8504d98a26b48
liao <liaomovie2@gmail.com>
2018-09-07 12:14:11 +0800

    automate scraping & build web site

--- render-PTTdict.sh
+++ render-PTTdict.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ori_dir=$PWD
+
 # Set Up virtualenv
 virtualenv -p python3 .
 source bin/activate
@@ -11,6 +13,10 @@ bash run.sh
 deactivate
 
 # Construct dictionary with R
+cd data
 Rscript -e 'source("dict_constr.R")' 
+Rscript -e 'rmarkdown::render_site(encoding = "UTF-8")'
+cp -r docs/ $ori_dir
 
+cd $ori_dir
 
