81fc2e65245525c164bf58afe5130b5de9f521c2
liao <liao961120@gmail.com>
2020-11-12 09:12:50 +0800

    tweak front-end

--- flatten_data.py
+++ flatten_data.py
@@ -1,33 +1,52 @@
 #%% 
 import os
 import json
+from datetime import date
 
 with open("data/dict.json") as f:
     data = json.load(f)
 with open("docs/dict.json", "w") as f:
-    json.dump(data, f, ensure_ascii=False)
+    json.dump(data, f, ensure_ascii=False, indent=True)
 
+with open("docs/last-updated.json", "w") as f:
+    json.dump([str(date.today())], f)
+
+
+def get_url(page):
+    for src, items in page.items():
+        if src == 'url':
+            return items[0]
+    raise Exception('No url found')
 #%%
 out = {}
 for page in data:
-    for src, items in page.items():
-        if items is None or src == 'url' or src == 'date': continue
+    url = get_url(page)
+    for cat, items in page.items():
+        if items is None or cat == 'url' or cat == 'date': continue
         
         for item in items:
             item = item.strip()
             if len(item) > 1:
-            	out[item] = src
-out = sorted( ((item, src) for item, src in out.items()), reverse=True)
+                if item not in out:
+                    out[item] = { 
+                        'category': cat,
+                        'src': url
+                    }
+                else:
+                    out[item]['category'] += f', {cat}'
+
+
+out = sorted( ((item, d['category'], d['src']) for item, d in out.items()), reverse=True)
 
 
 #%%
 # Save as json for web app
 with open("docs/ptt-terms.json", "w") as f:
-    json.dump(out, f, ensure_ascii=False)
+    json.dump(out, f, ensure_ascii=False, indent=True)
 
 
 # Save as tsv
 with open("docs/ptt-terms.tsv", "w") as f:
-    f.write('item\tsrc\n')
-    for item, src in out:
-        f.write(f'{item}\t{src}\n')
+    f.write('item\tcategory\tsrc\n')
+    for item, category, src in out:
+        f.write(f'{item}\t{category}\t{src}\n')
