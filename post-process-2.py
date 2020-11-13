092a65eaf6c20420444f2575e7ea63a6a9122edc
liao961120 <liao961120@gmail.com>
2020-11-13 14:59:37 +0800

    update

--- post-process-2.py
+++ post-process-2.py
@@ -24,7 +24,7 @@ with open(OUTPUT_DICT, "w") as f:
 out = to_publish_format(all_dict)
 # Save as json for web app
 with open(OUTPUT_VUE, "w") as f:
-    json.dump(out, f, ensure_ascii=False, indent=True)
+    json.dump(out, f, ensure_ascii=False)
 
 # Save as tsv
 with open(OUTPUT_TSV, "w") as f:
